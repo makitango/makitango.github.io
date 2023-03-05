5b0fb7f93c89a45327b7b0df63d0f2f6d79912af
makitango <dominic.bergmann@gmail.com>
2023-03-05 21:19:50 +0100

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,8 @@
-document.querySelector('button').addEventListener('click', function(){
-  document.querySelector('abc-def').style.color='black';
-})
+let colorBlock = document.querySelector("abc-def");
+let colors = colorBlock.querySelectorAll("li");
+let btn = document.querySelector("button");
+
+btn.addEventListener("click", function (e) {
+   e.preventDefault();
+   colors.forEach((li) => (li.style.color = "black"));
+});
